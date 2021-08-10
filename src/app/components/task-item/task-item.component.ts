53c007869e85a2b6dbe90237a51a38b6537e6495
I Made Wira <wirasatya19@gmail.com>
2021-08-10 16:09:59 +0800

    commit: tasks tracker angular app commit

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task!: Task;
+  @Output() deleteTask:EventEmitter<Task> = new EventEmitter()
+  @Output() toggleReminder:EventEmitter<Task> = new EventEmitter()
+  faTimes=faTimes;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  onDelete(task:Task){
+    this.deleteTask.emit(task)
+  }
+
+  onToggle(task:Task){
+    this.toggleReminder.emit(task)
+  }
+
+}
